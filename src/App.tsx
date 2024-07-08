fcc3186d6d11f5212ec20011e06f8588590c0463
Sandeep <sandeep@unifi.dev>
2024-07-08 17:16:35 +0530

    hamburger added

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Navbar } from "./component/Navbar";
 function App() {
   return (
     <>
-      <h1 className="text-xl font-bold text-red-400">
+      <h1 className="text-xl font-bold text-grey">
         {/* Hello Tailwind with react vite */}
         <Navbar />
         <AllRoutes />
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,9 +2,19 @@
 import React from "react";
 
 const Home: React.FC = () => {
+  const backgroundImageUrl =
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzzs-tRvcWkmLa51XWVWKOURbRtXCBZYbJAg&s";
+
   return (
-    <div>
-      <p>This is the Home page</p>
+    <div
+      className="h-screen bg-cover bg-center flex items-center justify-center"
+      style={{ backgroundImage: `url(${backgroundImageUrl})` }}
+    >
+      <div className="bg-black bg-opacity-50 p-8 rounded">
+        <p className="text-white text-2xl md:text-4xl font-bold">
+          This is the Home page
+        </p>
+      </div>
     </div>
   );
 };

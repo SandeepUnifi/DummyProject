2a2ca08b808623c429e3e2a5ad45a588b8cd993c
Sandeep <sandeep@unifi.dev>
2024-07-08 11:38:56 +0530

    routing added

--- src/component/AllRoutes.tsx
+++ src/component/AllRoutes.tsx
@@ -1,6 +1,18 @@
-import React from "react";
-import { Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import { Home } from "../pages/Home";
+import { PageNotfound } from "../pages/PageNotfound";
+import { Dashboard } from "../pages/Dashboard";
+import { Login } from "../pages/Login";
+import { Signup } from "../pages/Signup";
 
 export const AllRoutes = () => {
-  return <Routes>AllRoutes</Routes>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="*" element={<PageNotfound />} />
+    </Routes>
+  );
 };

f3b55a9f3902cbf97728828316d82d121ce0b172
Sandeep <sandeep@unifi.dev>
2024-07-08 11:03:04 +0530

    dummy project start

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-xl font-bold text-red-400">
+        Hello Tailwind with react vite
+      </h1>
+    </>
+  );
+}
+
+export default App;

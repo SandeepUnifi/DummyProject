3c9fd142da79b7dcbe6fe0f6dbb3132494e75170
Sandeep <sandeep@unifi.dev>
2024-07-08 11:33:06 +0530

    basic setup

---
+++ src/data.js
@@ -0,0 +1,38 @@
+export const data = [
+  {
+    id: 1,
+    name: "n1",
+    email: "e1",
+    department: "d1",
+    password: "p1",
+    avatar:
+      "https://static.vecteezy.com/system/resources/previews/028/794/707/non_2x/cartoon-cute-school-boy-photo.jpg",
+  },
+  {
+    id: 1,
+    name: "n1",
+    email: "e1",
+    department: "d1",
+    password: "p1",
+    avatar:
+      "https://static.vecteezy.com/system/resources/previews/028/794/707/non_2x/cartoon-cute-school-boy-photo.jpg",
+  },
+  {
+    id: 1,
+    name: "n1",
+    email: "e1",
+    department: "d1",
+    password: "p1",
+    avatar:
+      "https://static.vecteezy.com/system/resources/previews/028/794/707/non_2x/cartoon-cute-school-boy-photo.jpg",
+  },
+  {
+    id: 1,
+    name: "n1",
+    email: "e1",
+    department: "d1",
+    password: "p1",
+    avatar:
+      "https://static.vecteezy.com/system/resources/previews/028/794/707/non_2x/cartoon-cute-school-boy-photo.jpg",
+  },
+];
